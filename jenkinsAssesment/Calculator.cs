00922cc1030a9791ec898d50850b60abfb98e9da
abc <abc@gmail.com>
2024-02-15 15:14:06 +0530

    Add project files.

---
+++ jenkinsAssesment/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkinsAssesment
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Method to subtract two numbers
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ jenkinsAssesment/Program.cs
@@ -0,0 +1,36 @@
+﻿using jenkinsAssesment;
+using System;
+
+namespace jenkinsAssesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a Simple .NET Core Console Application!");
+
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+
+        
+            Console.WriteLine($"Hello, {userName}!");
+
+            Calculator calculator = new Calculator();
+
+            Console.Write("Enter a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = calculator.Add(number1, number2);
+            int difference = calculator.Subtract(number1, number2);
+
+            Console.WriteLine($"The sum of {number1} and {number2} is: {sum}");
+            Console.WriteLine($"The difference between {number1} and {number2} is: {difference}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
